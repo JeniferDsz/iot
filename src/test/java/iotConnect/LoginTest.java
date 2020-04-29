7e30fbbc3afb2dede89f3a99d9209ac4ca93e1b1
Jenifer Dsouza <jjendsouza@gmail.com>
2020-04-29 13:02:19 +0530

    Staging 1

--- src/test/java/iotConnect/LoginTest.java
+++ src/test/java/iotConnect/LoginTest.java
@@ -6,6 +6,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -18,29 +20,41 @@ import Resources.Base;
 
 
 public class LoginTest extends Base{
-
+		public static Logger log=LogManager.getLogger(Base.class.getName());
 		// TODO Auto-generated method stub
 		@Test(dataProvider= "getData")
 		public void loginiot(String username, String password) throws IOException, InterruptedException
 		{
+			
 			WebDriver driver=initializedriver();
+			log.debug("Started debugging");
 			Login l=new Login(driver);
+			log.info("driver is initialized");
 			try 
 			{
+				
 				l.register().click();
 				l.enterusername().sendKeys(username);
 				l.enteruserpass().sendKeys(password);
 				Thread.sleep(2000);
 				l.submit().click();
+				log.info("Home page is loaded");
 				Assert.assertTrue(l.code().isDisplayed());
 			//	Thread.sleep(2000);
 				
 			}
 			catch(Exception e)
 			{
+				
 				l.alertaccept().isDisplayed();
 				System.out.println(l.textdisplayed().getText());
-				l.clickalert().click();		
+				log.info("text is displayed");
+				l.clickalert().click();	
+				log.error("Wrong email or pssword entered");
+			}
+			catch(Error e)
+			{
+				log.error("Wrong email or pssword entered");
 			}
 			driver.close();
 		}
